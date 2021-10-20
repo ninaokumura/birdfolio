208881ca14e72e6e3741a55719cae271281f1267
Nina Okumura <elaineokumura@gmail.com>
2021-10-20 20:12:21 +1300

    add helper functions

---
+++ database/helpers.js
@@ -0,0 +1,25 @@
+const path = require('path')
+const fs = require('fs')
+const express = require('express')
+
+const filePath = path.join(__dirname, 'database', 'birds.json')
+
+function getAllBirds(cb) {
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      console.error('Failed to read file: ', err.message)
+      return
+    }
+    const birdsData = JSON.parse(data)
+    cb(birdsData.birds)
+  })
+}
+
+function getBirdById(id, cb) {
+  getAllBirds(birds => {
+    const foundBird = birds.find(element => element.id === id)
+    cb(foundBird)
+  })
+}
+
+module.export = helpers
--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const hbs = require('express-handlebars')
 
 const server = express()
 
+// Server configuration
+server.use(express.static('public'))
+server.use(express.urlencoded({ extended: false }))
+
 // Handlebars configuration
 server.engine('hbs', hbs({ extname: 'hbs' }))
 server.set('view engine', 'hbs')
